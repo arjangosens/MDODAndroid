e64d96f78d98f51133010eca8d7965906c567802
Arjan Gosens <arjangosens@hotmail.com>
2018-05-22 13:50:43 +0200

    Implemented DatePicker

--- app/src/main/java/com/project/avans/mdodandroid/RegisterActivity.java
+++ app/src/main/java/com/project/avans/mdodandroid/RegisterActivity.java
@@ -1,12 +1,14 @@
 package com.project.avans.mdodandroid;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
     private Button registerButton;
     private Button datePickerButton;
@@ -31,16 +33,27 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             case R.id.activityRegister_buttonDateOfBirth:
                 Log.i("RegisterActivity", "onClick of datePickerButton called");
 
-                //TODO: Create a DatePicker
+                DatePickerDialog dialog = new DatePickerDialog(this, this, 2018, 0, 1);
+                dialog.show();
 
                 break;
 
             case R.id.activityRegister_buttonCreateAccount:
                 Log.i("RegisterActivity", "onClick of registerButton called");
+
                 //TODO: Create intent to dashboard with new account
 
                 //TODO: Pass data through API and check if data is correct
                 break;
         }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        month = month + 1;
+        Log.i("RegisterActivity", "onDateSet called, date: " + dayOfMonth + "/" + month + "/" + year);
+        datePickerButton.setText(dayOfMonth + "/" + month + "/" + year);
+
+        //TODO: Pass date of birth through with the rest, and possibly add a check for invalid dates (like in the future)
     }
 }
