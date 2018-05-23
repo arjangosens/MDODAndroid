b10e193dbc3f87ca7903f46727b989260608467c
Arjan Gosens <arjangosens@hotmail.com>
2018-05-23 13:27:29 +0200

    Created methods for checking passwords
    The variant with currentPassword does not check on the currentPassword as of right now, will change in the future

--- app/src/main/java/com/project/avans/mdodandroid/applicationLogic/ValueChecker.java
+++ app/src/main/java/com/project/avans/mdodandroid/applicationLogic/ValueChecker.java
@@ -1,4 +1,65 @@
 package com.project.avans.mdodandroid.applicationLogic;
 
+import android.util.Log;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ValueChecker {
+    private static final String VALUECHECKERSTRING = "ValueChecker";
+
+    public static boolean checkPassword(String newPassword, String confirmPassword) {
+        Pattern pwRegex =
+                Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
+
+        Matcher matcher = pwRegex.matcher(newPassword);
+
+        boolean pwRegexResult = matcher.find();
+
+        if (pwRegexResult) {
+
+            if (newPassword.equals(confirmPassword)) {
+                Log.i(VALUECHECKERSTRING, "checkPassword() passwords are equal");
+                return true;
+
+            } else {
+                Log.i(VALUECHECKERSTRING, "checkPassword() passwords are NOT equal");
+                return false;
+
+            }
+
+        } else {
+            Log.i(VALUECHECKERSTRING, "checkPassword() password is invalid");
+            return false;
+        }
+    }
+
+    public static boolean checkPassword(String currentPassword, String newPassword, String confirmPassword) {
+
+        //TODO: Modify this method so that it checks if currentPassword is correct
+
+        Pattern pwRegex =
+                Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
+
+        Matcher matcher = pwRegex.matcher(newPassword);
+
+        boolean pwRegexResult = matcher.find();
+
+        if (pwRegexResult) {
+
+            if (newPassword.equals(confirmPassword)) {
+                Log.i(VALUECHECKERSTRING, "checkPassword() passwords are equal");
+                return true;
+
+            } else {
+                Log.i(VALUECHECKERSTRING, "checkPassword() passwords are NOT equal");
+                return false;
+
+            }
+
+        } else {
+            Log.i(VALUECHECKERSTRING, "checkPassword() password is invalid");
+            return false;
+        }
+    }
 }
