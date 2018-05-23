a9c72beb48c99f75bbe3dd5bd4151c9dcc4bda38
Kelly <knzegela@avans.nl>
2018-05-23 14:34:45 +0200

    now uses string values from res folder

--- app/src/main/java/com/project/avans/mdodandroid/UserSettingsActivity.java
+++ app/src/main/java/com/project/avans/mdodandroid/UserSettingsActivity.java
@@ -10,15 +10,32 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class UserSettingsActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private ListView settingsListview;
-
-    private String[] settings = {"First Name", "insertion", "Last Name", "Date of birth", "Email", "Password"};
+    private ArrayList<String> settings = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
+
+        String firstName = getResources().getString(R.string.firstName);
+        String insertion = getResources().getString(R.string.Insertion);
+        String lastName = getResources().getString(R.string.Lastname);
+        String dateOfBirth = getResources().getString(R.string.Dateofbirth);
+        String email = getResources().getString(R.string.Email);
+        String password = getResources().getString(R.string.password);
+
+        //TODO: add local user data
+
+        settings.add(firstName);
+        settings.add(insertion);
+        settings.add(lastName);
+        settings.add(dateOfBirth);
+        settings.add(email);
+        settings.add(password);
 
         //TODO: connect the Textviews to the userdata
 
@@ -41,10 +58,11 @@ public class UserSettingsActivity extends AppCompatActivity implements AdapterVi
         builder.setTitle("Change " + type);
 
         switch (type) {
-            case ("Email"):
+            case ("Emailadres"):
+            case ("Email address"):
                 builder.setView(inflater.inflate(R.layout.dialog_updateprofile_email, null));
                 break;
-
+            case ("Wachtwoord"):
             case ("Password"):
                 builder.setView(inflater.inflate(R.layout.dialog_updateprofile_password, null));
                 break;
@@ -73,7 +91,7 @@ public class UserSettingsActivity extends AppCompatActivity implements AdapterVi
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        showUpdateDialog(settings[position]);
+        showUpdateDialog(settings.get(position));
 
     }
 }
