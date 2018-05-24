991cbac17f48580c09844bf6079e726e9d557716
Arjan Gosens <arjangosens@hotmail.com>
2018-05-24 13:32:51 +0200

    checkNewPasswordFormat() and checkConfirmMatchesNewPassword() now work

--- app/src/main/java/com/project/avans/mdodandroid/applicationLogic/ValueChecker.java
+++ app/src/main/java/com/project/avans/mdodandroid/applicationLogic/ValueChecker.java
@@ -38,15 +38,28 @@ public class ValueChecker {
         return true;
     }
 
-    public static boolean checkNewPasswordFormat() {
-        return true;
+    public static boolean checkNewPasswordFormat(String newPassword) {
+        Pattern pwRegex =
+                Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
+
+        Matcher matcher = pwRegex.matcher(newPassword);
+
+        return matcher.find();
     }
 
-    public static boolean checkConfirmMatchesNewPassword() {
-        return true;
+    public static boolean checkConfirmMatchesNewPassword(String password, String confirmPassword) {
+        if (password.equals(confirmPassword)) {
+            Log.i(VALUECHECKERSTRING, "checkPassword() passwords are equal");
+            return true;
+
+        } else {
+            Log.i(VALUECHECKERSTRING, "checkPassword() passwords are NOT equal");
+            return false;
+        }
     }
 
-    public static boolean checkPassword(String currentPassword, String newPassword, String confirmPassword) {
+    public static boolean checkPassword(String currentPassword, String newPassword, String
+            confirmPassword) {
 
         //TODO: Modify this method so that it checks if currentPassword is correct
 
@@ -79,7 +92,7 @@ public class ValueChecker {
         Pattern emailRegex =
                 Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE);
 
-        Matcher matcher = emailRegex .matcher(email);
+        Matcher matcher = emailRegex.matcher(email);
 
         boolean result = matcher.find();
         Log.i("RegisterActivity", "checkEmail email is " + result);
