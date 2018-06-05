6f5fc9dfd90a62a6570a5ecec2ddb584aad953c3
Arjan Gosens <arjangosens@hotmail.com>
2018-06-05 15:04:10 +0200

    Created new Activity for displaying usage on a specific day

---
+++ app/src/main/java/com/project/avans/mdodandroid/ConsumptionSpecificDayActivity.java
@@ -0,0 +1,13 @@
+package com.project.avans.mdodandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsumptionSpecificDayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consumption_specific_day);
+    }
+}
