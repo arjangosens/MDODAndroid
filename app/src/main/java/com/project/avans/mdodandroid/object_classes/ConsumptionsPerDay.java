7674d338556f65b146a0c56e85414225799503ca
Arjan Gosens <arjangosens@hotmail.com>
2018-06-05 12:59:23 +0200

    Filled add() method

--- app/src/main/java/com/project/avans/mdodandroid/object_classes/ConsumptionsPerDay.java
+++ app/src/main/java/com/project/avans/mdodandroid/object_classes/ConsumptionsPerDay.java
@@ -1,9 +1,12 @@
 package com.project.avans.mdodandroid.object_classes;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 
 public class ConsumptionsPerDay {
+    final String TAG = "ConsumptionsPerDay";
     private Calendar date;
     private ArrayList<Consumption> consumptions;
 
@@ -13,7 +16,18 @@ public class ConsumptionsPerDay {
     }
 
     public void add(Consumption consumption) {
+        Calendar consumptionDate = consumption.getTimestamp();
 
+        if (consumptionDate.get(Calendar.YEAR) == date.get(Calendar.YEAR)
+                && consumptionDate.get(Calendar.MONTH) == date.get(Calendar.MONTH)
+                && consumptionDate.get(Calendar.DAY_OF_MONTH) == date.get(Calendar.DAY_OF_MONTH)) {
+
+            consumptions.add(consumption);
+            Log.i(TAG, "consumption added");
+
+        } else {
+            Log.i(TAG, "consumption NOT added, date does NOT match!");
+        }
     }
 
     public Calendar getDate() {
