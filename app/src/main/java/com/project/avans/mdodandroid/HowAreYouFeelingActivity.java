283e9acb4f77bd7ca6fe4c7f278f1fc6a9917ccf
Kelly <knzegela@avans.nl>
2018-06-06 22:13:14 +0200

    added the confirm button

--- app/src/main/java/com/project/avans/mdodandroid/HowAreYouFeelingActivity.java
+++ app/src/main/java/com/project/avans/mdodandroid/HowAreYouFeelingActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class HowAreYouFeelingActivity extends AppCompatActivity implements View.OnClickListener {
@@ -13,6 +15,9 @@ public class HowAreYouFeelingActivity extends AppCompatActivity implements View.
     private ImageView smileyOk;
     private ImageView smileySad;
     private ImageView smileyTerrible;
+    private Button buttonConfirm;
+    private TextView description;
+    private TextView error;
 
     //create final strings because the emotions stay the same
     final String HAPPY = "Happy";
@@ -46,6 +51,41 @@ public class HowAreYouFeelingActivity extends AppCompatActivity implements View.
         smileyOk.setTag(OK);
         smileySad.setTag(SAD);
         smileyTerrible.setTag(TERRIBLE);
+
+        //find descriptionField to send the description to the api when the confirm button is pressed
+        description = findViewById(R.id.editText_feeling_description);
+
+        //find the error textfield to show errors on the screen;
+        error = findViewById(R.id.textview_feeling_error);
+
+        //find confirm button to send data input to the api
+        buttonConfirm = findViewById(R.id.button_feeling_confirm);
+        buttonConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String field = String.valueOf(description.getText());
+
+                if(field.equals("")){
+                    error.setText(getResources().getString(R.string.descriptionInvalid));
+                }
+
+                if(smileyHappy.isSelected()){
+                    Log.i("IMAGE SELECTED: ", "1");
+                } else if (smileyGood.isSelected()){
+                    Log.i("IMAGE SELECTED: ", "2");
+                } else if (smileyOk.isSelected()){
+                    Log.i("IMAGE SELECTED: ", "3");
+                } else if (smileySad.isSelected()) {
+                    Log.i("IMAGE SELECTED: ", "4");
+                } else if (smileyTerrible.isSelected()){
+                    Log.i("IMAGE SELECTED: ", "5");
+                } else {
+                    Log.i("IMAGE SELECTED: ", "NO IMAGE SELECTED");
+                    error.setText(getResources().getString(R.string.smileyNotSelected));
+                }
+            }
+        });
+
     }
 
     // onclicklistener for the smileys
@@ -56,7 +96,7 @@ public class HowAreYouFeelingActivity extends AppCompatActivity implements View.
         //get the tag when a image is clicked
         String emotion = String.valueOf(v.getTag());
 
-        int color = getResources().getColor(R.color.coloraccent);
+        int color = getResources().getColor(R.color.selected);
 
 
         //this selects the current smiley and deselects the other smileys
