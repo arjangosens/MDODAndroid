a49541a68d87a7efd22b3db443ece7cd5a554df1
Arjan Gosens <arjangosens@hotmail.com>
2018-06-06 13:49:12 +0200

    Textviews in DetailActivity are now filled (except amountType)

--- app/src/main/java/com/project/avans/mdodandroid/ConsumptionDetailActivity.java
+++ app/src/main/java/com/project/avans/mdodandroid/ConsumptionDetailActivity.java
@@ -2,12 +2,42 @@ package com.project.avans.mdodandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.project.avans.mdodandroid.object_classes.Consumption;
 
 public class ConsumptionDetailActivity extends AppCompatActivity {
+    private Consumption consumption;
+
+    private TextView typeTextView;
+    private TextView timestampTextView;
+    private TextView amountValueTextView;
+    private TextView amountTypeTextView;
+    private TextView descriptionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consumption_detail);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            consumption = (Consumption) extras.get(ConsumptionSpecificDayActivity.CONSUMPTION);
+        }
+
+        typeTextView = findViewById(R.id.activityConsumptionDetail_textViewHeader);
+        timestampTextView = findViewById(R.id.act_consumptionDetail_textViewTimeStampField);
+        amountValueTextView = findViewById(R.id.act_consumptionDetail_textViewAmountValueField);
+        amountTypeTextView = findViewById(R.id.act_consumptionDetail_textViewAmountTypeField);
+        descriptionTextView = findViewById(R.id.act_consumptionDetail_textViewDescField);
+
+        typeTextView.setText(consumption.getType());
+        amountValueTextView.setText(String.valueOf(consumption.getAmount()));
+        timestampTextView.setText(consumption.getDate().toString());
+        descriptionTextView.setText(consumption.getDescription());
+
+
     }
 }
