76004676d6344802d185ff88e6aca4493afc6d38
Arjan Gosens <arjangosens@hotmail.com>
2018-06-06 13:06:31 +0200

    Created Consumption Detail Activity + lay-out

---
+++ app/src/main/java/com/project/avans/mdodandroid/ConsumptionDetailActivity.java
@@ -0,0 +1,13 @@
+package com.project.avans.mdodandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsumptionDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consumption_detail);
+    }
+}
