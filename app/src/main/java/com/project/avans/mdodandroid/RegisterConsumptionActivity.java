0e7acc456c1968aed4e87da659e4eb261f3350a1
Arjan Gosens <arjangosens@hotmail.com>
2018-06-07 13:45:34 +0200

    RecyclerView now uses adapter

--- app/src/main/java/com/project/avans/mdodandroid/RegisterConsumptionActivity.java
+++ app/src/main/java/com/project/avans/mdodandroid/RegisterConsumptionActivity.java
@@ -2,14 +2,19 @@ package com.project.avans.mdodandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ListView;
+
+import com.project.avans.mdodandroid.consumptionAdapter.ConRegSubstanceAdapter;
 import com.project.avans.mdodandroid.object_classes.Substance;
 
 import java.util.ArrayList;
 
 public class RegisterConsumptionActivity extends AppCompatActivity {
-    private ListView substanceListView;
+    private RecyclerView substanceRv;
     private ArrayList<Substance> substances;
+    private ConRegSubstanceAdapter conRegSubstanceAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,15 @@ public class RegisterConsumptionActivity extends AppCompatActivity {
         substances = new ArrayList<>();
         substances.add(new Substance("Alcohol", getResources().getDrawable(R.drawable.wine), "ml"));
         substances.add(new Substance("Alcohol2", getResources().getDrawable(R.drawable.wine), "ml"));
+
+        substanceRv = (RecyclerView) findViewById(R.id.act_registerConsumption_RecyclerViewSubstances);
+
+        conRegSubstanceAdapter = new ConRegSubstanceAdapter(substances);
+        substanceRv.setAdapter(conRegSubstanceAdapter);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.HORIZONTAL);
+        substanceRv.setLayoutManager(llm);
 
 
 
