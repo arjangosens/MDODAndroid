f0cad0ccda86d3a1273e638c6bdbaadc795c2cf4
Arjan Gosens <arjangosens@hotmail.com>
2018-06-07 11:36:09 +0200

    Modified Substance domain object for usage in consumption

--- app/src/main/java/com/project/avans/mdodandroid/object_classes/Substance.java
+++ app/src/main/java/com/project/avans/mdodandroid/object_classes/Substance.java
@@ -1,12 +1,22 @@
 package com.project.avans.mdodandroid.object_classes;
 
+import android.graphics.drawable.Drawable;
+
 public class Substance {
     private String id;
     private String type;
+    private Drawable drawable;
+    private String measurement;
 
     public Substance(String id, String type) {
         this.id = id;
         this.type = type;
+    }
+
+    public Substance(String type, Drawable drawable, String measurement) {
+        this.type = type;
+        this.drawable = drawable;
+        this.measurement = measurement;
     }
 
     public String getId() {
@@ -15,5 +25,29 @@ public class Substance {
 
     public String getType() {
         return type;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Drawable getDrawable() {
+        return drawable;
+    }
+
+    public void setDrawable(Drawable drawable) {
+        this.drawable = drawable;
+    }
+
+    public String getMeasurement() {
+        return measurement;
+    }
+
+    public void setMeasurement(String measurement) {
+        this.measurement = measurement;
     }
 }
