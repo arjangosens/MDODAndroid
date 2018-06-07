820d4eac788466c7007d9b8170ad2fc2a532494b
Arjan Gosens <arjangosens@hotmail.com>
2018-06-07 11:58:34 +0200

    ListView now uses adapter (but scrolls in wrong direction!)

--- app/src/main/java/com/project/avans/mdodandroid/RegisterConsumptionActivity.java
+++ app/src/main/java/com/project/avans/mdodandroid/RegisterConsumptionActivity.java
@@ -2,12 +2,31 @@ package com.project.avans.mdodandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.project.avans.mdodandroid.consumptionAdapter.ConRegisterSubstanceAdapter;
+import com.project.avans.mdodandroid.object_classes.Substance;
+
+import java.util.ArrayList;
 
 public class RegisterConsumptionActivity extends AppCompatActivity {
+    private ListView substanceListView;
+    private ArrayList<Substance> substances;
+    private ConRegisterSubstanceAdapter conRegisterSubstanceAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_consumption);
+
+        substances = new ArrayList<>();
+        substances.add(new Substance("Alcohol", getResources().getDrawable(R.drawable.wine), "ml"));
+        substances.add(new Substance("Alcohol2", getResources().getDrawable(R.drawable.wine), "ml"));
+
+        substanceListView = (ListView) findViewById(R.id.act_registerConsumption_listViewTypes);
+        conRegisterSubstanceAdapter = new ConRegisterSubstanceAdapter(getLayoutInflater(), substances);
+        substanceListView.setAdapter(conRegisterSubstanceAdapter);
+
+
     }
 }
