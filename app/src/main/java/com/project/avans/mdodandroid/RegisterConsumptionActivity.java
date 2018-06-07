c2f59c1b1f7c5f9dd0e6e92deafe0ddb7bf68f87
Arjan Gosens <arjangosens@hotmail.com>
2018-06-07 12:31:39 +0200

    Replaced ListView with RecyclerView, removed adapter

--- app/src/main/java/com/project/avans/mdodandroid/RegisterConsumptionActivity.java
+++ app/src/main/java/com/project/avans/mdodandroid/RegisterConsumptionActivity.java
@@ -3,8 +3,6 @@ package com.project.avans.mdodandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
-
-import com.project.avans.mdodandroid.consumptionAdapter.ConRegisterSubstanceAdapter;
 import com.project.avans.mdodandroid.object_classes.Substance;
 
 import java.util.ArrayList;
@@ -12,7 +10,6 @@ import java.util.ArrayList;
 public class RegisterConsumptionActivity extends AppCompatActivity {
     private ListView substanceListView;
     private ArrayList<Substance> substances;
-    private ConRegisterSubstanceAdapter conRegisterSubstanceAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +20,7 @@ public class RegisterConsumptionActivity extends AppCompatActivity {
         substances.add(new Substance("Alcohol", getResources().getDrawable(R.drawable.wine), "ml"));
         substances.add(new Substance("Alcohol2", getResources().getDrawable(R.drawable.wine), "ml"));
 
-        substanceListView = (ListView) findViewById(R.id.act_registerConsumption_listViewTypes);
-        conRegisterSubstanceAdapter = new ConRegisterSubstanceAdapter(getLayoutInflater(), substances);
-        substanceListView.setAdapter(conRegisterSubstanceAdapter);
+
 
 
     }
