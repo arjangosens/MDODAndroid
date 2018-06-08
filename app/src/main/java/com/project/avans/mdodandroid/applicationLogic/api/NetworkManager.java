47b7212a8094c376cfa3511b907a6299dc6c9a37
Kelly <knzegela@avans.nl>
2018-06-08 10:37:10 +0200

    Intent works

--- app/src/main/java/com/project/avans/mdodandroid/applicationLogic/api/NetworkManager.java
+++ app/src/main/java/com/project/avans/mdodandroid/applicationLogic/api/NetworkManager.java
@@ -790,4 +790,90 @@ public class NetworkManager
 
         requestQueue.add(request);
     }
+
+    public void getEmotionStatusDays(final VolleyListener<JSONObject> listener) {
+        String url = prefixURL + "v1/mood/status";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response)
+                    {
+                        Log.d(TAG + ": ", "GET client Response : " + response.toString());
+                        if(null != response.toString())
+                            listener.getResult(response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        if (null != error.networkResponse)
+                        {
+                            Log.d(TAG + ": ", "Error Response code: " + error.networkResponse.statusCode);
+                            listener.getResult(null);
+                        }
+                    }
+                }) {@Override
+        public Map<String, String> getHeaders() throws AuthFailureError {
+            Map<String, String> params = new HashMap<String, String>();
+            Log.i(TAG, "Mainactivity.Token = " + MainActivity.Token);
+            params.put("Authorization", "Bearer " + MainActivity.Token);
+            params.put("X-Access-Token", MainActivity.Token);
+            params.put("Content-Type", "application/json");
+
+            return params;
+        }};
+
+        requestQueue.add(request);
+    }
+
+
+//    public void postStatus(String substanceId, String lust, String description, String prevention, final VolleyListener<JSONObject> listener) {
+//
+//        String url = prefixURL + "v1/difficult_moment";
+//
+//        Map<String, Object> jsonParams = new HashMap<>();
+//        jsonParams.put("prevention", prevention);
+//        jsonParams.put("substance_id", substanceId);
+//        jsonParams.put("lust", lust);
+//        jsonParams.put("description", description);
+//
+//        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(jsonParams),
+//                new Response.Listener<JSONObject>()
+//                {
+//                    @Override
+//                    public void onResponse(JSONObject response)
+//                    {
+//                        Log.d(TAG + ": ", "post moment Response : " + response.toString());
+//                        if(null != response.toString())
+//                            listener.getResult(response);
+//                    }
+//                },
+//                new Response.ErrorListener()
+//                {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error)
+//                    {
+//                        if (null != error.networkResponse)
+//                        {
+//                            Log.d(TAG + ": ", "Error Response code: " + error.networkResponse.statusCode);
+//                            listener.getResult(null);
+//                        }
+//                    }
+//                }){@Override
+//        public Map<String, String> getHeaders() throws AuthFailureError {
+//            Map<String, String> params = new HashMap<String, String>();
+//            Log.i(TAG, "Mainactivity.Token = " + MainActivity.Token);
+//            params.put("Authorization", "Bearer " + MainActivity.Token);
+//            params.put("X-Access-Token", MainActivity.Token);
+//            params.put("Content-Type", "application/json");
+//
+//            return params;
+//        }};
+//        requestQueue.add(request);
+//    }
+
 }
