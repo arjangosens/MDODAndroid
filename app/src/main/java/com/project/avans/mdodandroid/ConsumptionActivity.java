20a70cd2c6cb5baaa0815ef3d3375b4d897da270
Unknown <mvos20@avans.nl>
2018-06-10 20:35:17 +0200

    Merge branch 'consumption' into develop

--- app/src/main/java/com/project/avans/mdodandroid/ConsumptionActivity.java
+++ app/src/main/java/com/project/avans/mdodandroid/ConsumptionActivity.java
@@ -1,7 +1,16 @@
 package com.project.avans.mdodandroid;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.nfc.Tag;
+import android.os.Build;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -36,11 +45,14 @@ public class ConsumptionActivity extends AppCompatActivity implements AdapterVie
 
     private ListView cpdListView;
     private Button registerButton;
+    Context context;
+    private String channelId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consumption);
+        context = this;
 
         consumptionsPerDayArrayList = new ArrayList<>();
         date = Calendar.getInstance().getTime();
@@ -53,6 +65,15 @@ public class ConsumptionActivity extends AppCompatActivity implements AdapterVie
         consumptionAdapter = new ConsumptionAdapter(getLayoutInflater(), consumptionsPerDayArrayList);
         cpdListView.setAdapter(consumptionAdapter);
         cpdListView.setOnItemClickListener(this);
+
+        Button btn = (Button) findViewById(R.id.nothingUsedButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notificat(getNotification("U Heeft al 2 dagen geen gebruik ingevoerd, voer uw gebruik in alstublieft."), 60 * 1000);//for showing
+
+            }
+        });
 
         registerButton = findViewById(R.id.activityConsumption_buttonRegisterConsumption);
         registerButton.setOnClickListener(this);
@@ -183,6 +204,54 @@ public class ConsumptionActivity extends AppCompatActivity implements AdapterVie
         Intent i = new Intent(getApplicationContext(), HomepageActivity.class);
         i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(i);
+    }
+
+
+    public void Notificat(Notification notification, int delay) {
+
+
+        Intent notificationIntent = new Intent(context, NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+
+    private Notification getNotification(String content) {
+        if (Build.VERSION.SDK_INT >= 26) {
+            channel();
+            Notification.Builder builder = new Notification.Builder(context, channelId);
+            builder.setContentTitle("Tactus Bericht");
+            builder.setContentText(content);
+            builder.setSmallIcon(R.drawable.tactuslogo_small_round);
+            return builder.build();
+        }
+        else{
+            Notification.Builder builder = new Notification.Builder(context);
+            builder.setContentTitle("Tactus Bericht");
+            builder.setContentText(content);
+            builder.setSmallIcon(R.drawable.tactuslogo_small_round);
+            return builder.build();
+        }
+    }
+    private void channel() {
+        if (Build.VERSION.SDK_INT >= 26) {
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            channelId = "Tactus channel Id";
+            CharSequence channelName = "Tactus";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
     }
 
 }
