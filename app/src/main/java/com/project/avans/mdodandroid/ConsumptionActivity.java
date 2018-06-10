a58ca4d315a259a3cf2c9722a59623c406f5b46f
Unknown <mvos20@avans.nl>
2018-06-10 13:47:27 +0200

    layout changes

--- app/src/main/java/com/project/avans/mdodandroid/ConsumptionActivity.java
+++ app/src/main/java/com/project/avans/mdodandroid/ConsumptionActivity.java
@@ -3,6 +3,7 @@ package com.project.avans.mdodandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -70,6 +71,8 @@ public class ConsumptionActivity extends AppCompatActivity implements AdapterVie
 
     @Override
     public void onClick(View v) {
+       Intent intent = new Intent(getApplicationContext(), RegisterConsumptionActivity.class);
+       startActivity(intent);
         
     }
 }
